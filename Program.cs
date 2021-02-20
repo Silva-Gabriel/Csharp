84195b5aef0cc18fd6089f5853523762c9ce45dd
pottergryff8@gmail.com <GABRIELDASILVASANTAN@GSS>
2021-02-20 03:05:16 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DIO_DOT_NET
+{
+    class Program
+    {
+        static void Main()
+        {
+           int[] a = new int[5];
+        for(int i = 0;i < 5;i++)
+            a[i] =  Int32.Parse(Console.ReadLine());
+        for(int i = 0; i < 5; i++)
+            Console.Write(a[i]+" ");
+        }
+        
+
+    }
+}
