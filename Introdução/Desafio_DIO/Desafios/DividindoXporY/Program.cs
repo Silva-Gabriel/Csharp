e5242130d42240b96d5da27cd73d10ffb99315cd
pottergryff8@gmail.com <pottergryff8@gmail.com>
2021-04-28 22:07:03 -0300

    Desafio dividindo X por Y

---
+++ Introdução/Desafio_DIO/Desafios/DividindoXporY/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DividindoXporY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++) 
+            {
+                int X = Int32.Parse(Console.ReadLine());
+                int Y = Int32.Parse(Console.ReadLine());
+                if(Y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else if(X == 0)
+                {
+                    Console.WriteLine("0.0");
+                }
+                else if(Y != 0)
+                {
+                    double divisao = X/Y;
+                    Console.WriteLine(divisao.ToString(""));
+                }
+            }
+        }
+    }
+}
